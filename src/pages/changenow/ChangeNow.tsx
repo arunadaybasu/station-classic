b2f2e6c69565e625985b5b31ad1ccd0d8be04e3b
Arunaday Basu <arunadaybasu@gmail.com>
2024-03-14 18:23:47 +0530

    Added Middleware API for Exchange

--- src/pages/changenow/ChangeNow.tsx
+++ src/pages/changenow/ChangeNow.tsx
@@ -1,13 +1,14 @@
-import { useTranslation } from "react-i18next"
-import { Card, Page } from "components/layout"
-import { useMemo, useState } from "react"
+// import { useTranslation } from "react-i18next"
+// import { Card, Page } from "components/layout"
+import { useMemo, useState, useEffect } from "react"
 import { Select, Option } from "bymax-react-select"
+import axios from "axios"
 // import styles from "./ChangeNow.module.scss"
 
 const ChangeNow = () => {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
-  const options: Option[] = useMemo(
+  var optionsFrom: Option[] = useMemo(
     () => [
       {
         id: "1",
@@ -32,19 +33,84 @@ const ChangeNow = () => {
     []
   )
 
-  const [value, setValue] = useState<Option | Option[] | null>(options[0])
+  var optionsTo: Option[] = useMemo(
+    () => [
+      {
+        id: "1",
+        value: "BTC",
+        label: "Bitcoin",
+        image: "https://content-api.changenow.io/uploads/btc_1_527dc9ec3c.svg",
+        base: "BTC",
+        quote: "USDT",
+      },
+      {
+        id: "2",
+        value: "eth",
+        label: "Ethereum",
+        base: "ETH",
+      },
+      {
+        id: "3",
+        value: "sol",
+        label: "Solana",
+      },
+    ],
+    []
+  )
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+  const [valueFrom, setValueFrom] = useState<Option | Option[] | null>(
+    optionsFrom[0]
+  )
+  const [valueTo, setValueTo] = useState<Option | Option[] | null>(optionsTo[0])
+
+  useEffect(() => {
+    const fetchDataFrom = async () => {
+      setLoading(true)
+      try {
+        const { data: response } = await axios.get(
+          "http://localhost:5737/exchangeapi/changenow/currencies"
+        )
+        setData(response.result)
+        console.log(response.result)
+        for (var i = 0; response.result.length; i++) {
+          optionsFrom.push({
+            id: i + 1 + "",
+            value: response.result[i].ticker,
+            label: response.result[i].name,
+            image: response.result[i].image,
+          })
+        }
+      } catch (error) {
+        console.error(error.message)
+      }
+      setLoading(false)
+    }
+
+    fetchDataFrom()
+  }, [optionsFrom])
 
   return (
     <div style={{ width: "300px" }}>
       <Select
-        id="exchange-from"
-        value={value}
+        id="exchange-from-currency"
+        value={valueFrom}
         isMulti={false}
         isClearable={true}
-        options={options}
+        options={optionsFrom}
         placeholder="Select a coin"
         noOptionsMessage="No coins found"
-        onChange={(selectedOption) => setValue(selectedOption)}
+        onChange={(selectedOption) => setValueFrom(selectedOption)}
+      />
+      <Select
+        id="exchange-to-currency"
+        value={valueTo}
+        isMulti={false}
+        isClearable={true}
+        options={optionsTo}
+        placeholder="Select a coin"
+        noOptionsMessage="No coins found"
+        onChange={(selectedOption) => setValueTo(selectedOption)}
       />
     </div>
   )
